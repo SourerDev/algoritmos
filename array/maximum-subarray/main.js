6fe7babc3f3c3b161f653931eca9ce408dda061e
jhoniernem <jhonny30ca@gmail.com>
2023-04-08 13:46:11 -0500

    new algorithm

---
+++ array/maximum-subarray/main.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+export default function(nums) {
+    let maxSum = nums[0]
+    let curSum = 0
+
+    for( i in nums){
+        if(curSum < 0) curSum = 0
+        curSum += nums[i];
+        maxSum = Math.max(maxSum,curSum)
+    }
+
+    return maxSum
+};
