cc3a8b76fe28915dd8dbe8d0b075936660d9e550
jhoniernem <jhonny30ca@gmail.com>
2023-04-23 19:28:10 -0500

    fix

--- array/maximum-subarray/main.js
+++ array/maximum-subarray/main.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 export default function maximumSubarray(nums) {
+    if(!nums?.length) return 0
+
     let maxSum = nums[0]
     let curSum = 0
 
-    for( i in nums){
+    for(let i in nums){
         if(curSum < 0) curSum = 0
         curSum += nums[i];
         maxSum = Math.max(maxSum,curSum)
