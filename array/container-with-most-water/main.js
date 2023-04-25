67b8573e77a235add3664f18d068c15b8e290863
jhoniernem <jhonny30ca@gmail.com>
2023-04-25 13:45:09 -0500

    add solution

---
+++ array/container-with-most-water/main.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+export default function maxArea(height) {
+  let max = 0;
+  let i = 0;
+  let j = height.length - 1;
+
+  while (i < j) {
+    const area = Math.min(height[i], height[j]) * (j - i);
+    max = Math.max(max, area);
+
+    if (height[i] <= height[j]) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+
+  return max;
+}
