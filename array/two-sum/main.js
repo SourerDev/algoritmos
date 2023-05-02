f4d1c44d8c6972e060e70e337e1cd493a65407d6
jhoniernem <jhonny30ca@gmail.com>
2023-05-02 13:43:40 -0500

    add comments

--- array/two-sum/main.js
+++ array/two-sum/main.js
@@ -1,4 +1,9 @@
-function twoSum(nums, target) {
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+export default function twoSum(nums, target) {
   const map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
@@ -14,5 +19,3 @@ function twoSum(nums, target) {
 const NUMS = [2, 7, 11, 15]
 const TARGET = 9
 twoSum(NUMS, TARGET);
-
-export default twoSum
