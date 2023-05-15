f8a55c7b937aabee1c4a175550d764323ad8ba2f
jhoniernem <jhonny30ca@gmail.com>
2023-05-15 16:31:56 -0500

    feat  : solved

--- binary/sum-of-two-integers/main.js
+++ binary/sum-of-two-integers/main.js
@@ -3,6 +3,17 @@
  * @param {number} b
  * @return {number}
  */
-export function getSum(a, b) {
-    
+function getSum(a, b) {
+  while (b !== 0) {
+    let carry = a & b;
+    a = a ^ b;
+    b = carry << 1;
 }
+
+  return a;
+}
+
+console.log(getSum(7, 3));
+module.exports = {
+  getSum,
+};
